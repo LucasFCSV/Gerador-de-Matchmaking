aad5f51513576faa2b7b34ba27a2b4b07c6b54e5
Lucas <lucasfsilva.dev@gmail.com>
2026-05-20 00:51:39 -0300

    Criado nova database para deixar mais interessante. Também, caso o úsuario digite o número inválido do rank retornará e poderá digitar novamente

--- principal.py
+++ principal.py
@@ -1,28 +1,6 @@
 import time
 import random
-
-nomes = [
-    "Lucasplay - 2 estrelas - Duelista",
-    "Jeffinhogames - 5 estrelas - Controlador",
-    "PedrinFPS - 4 estrelas - Sentinela",
-    "GuiZera - 3 estrelas - Iniciador",
-    "LukinhasBR - 5 estrelas - Duelista",
-    "JapaGamer - 1 estrela - Controlador",
-    "MatheusX1 - 4 estrelas - Flex",
-    "KauanPlay - 3 estrelas - Sentinela",
-    "RafaKills - 5 estrelas - Duelista",
-    "VictorRush - 2 estrelas - Iniciador",
-    "BruninAim - 4 estrelas - Duelista",
-    "Zezin - 3 estrelas - Controlador",
-    "Joaozin - 5 estrelas - Sentinela",
-    "LeoHeadshot - 2 estrelas - Flex",
-    "NandoClutch - 4 estrelas - Iniciador",
-    "GuiDoValorant - 5 estrelas - Duelista",
-    "JoaozinhoFPS - 3 estrelas - Sentinela",
-    "DaviRush - 1 estrela - Controlador",
-    "TutuSniper - 4 estrelas - Iniciador",
-    "HenriqueGG - 5 estrelas - Flex"]
-sample = random.sample(nomes, 4)
+from ranks import ferro, bronze, prata, ouro, platina, diamante, ascendente, imortal, radiante, todosRanks
 
 print("NÚMERO DE CADA RANK: " \
 "\n1 - Ferro " \
@@ -34,33 +12,31 @@ print("NÚMERO DE CADA RANK: " \
 "\n7 - Ascendente " \
 "\n8 - Imortal " \
 "\n9 - Radiante")
-rank = int(input("Digite o número do seu rank: "))
+
+rankUsuario = int(input("Digite o número do seu rank: "))
+
 
 while True:
+    if rankUsuario in todosRanks:
 
-    sample = random.sample(nomes, 4)
+        sample = random.sample(todosRanks[rankUsuario], 4)
+        print("\nSeu time: ")
+        print("\n".join(sample))
+            
+        while True:
+            confirmar = input("\nEsse é seu time, deseja continuar? (s/n) ")
+            
+            if confirmar == "s":
+                break
 
-    print("\nProcurando Teammates...")
-    time.sleep(2)
+            if confirmar == "n":
+                break
 
-    print("\nSeu time:")
-    print("\n".join(sample))
-   
-    while True:
+            if confirmar != "s" or confirmar != "n":
+                print("COMANDO INVÁLIDO")
 
-        continuar = input("\nDeseja continuar com esse time? (s/n): ").lower()
+    else:
+        print("Rank inválido!")
 
-        if continuar == "s":
-            print("Prosseguindo para o lobby")
-            break
-
-        if continuar == "n":
-            print("Procuraremos outro time")
-            time.sleep(2)
-            break
-
-        if continuar != "s" and continuar != "n":
-            print('Resposta inválida! Digite apenas "s" ou "n"')
-
-    if continuar == "s": 
+    if confirmar == "s":
         break
---
+++ ranks.py
@@ -0,0 +1,127 @@
+ferro = [
+    "Lucasplay - 9/10 - Duelista - Ferro 1",
+    "GuiZera - 2/10 - Iniciador - Ferro 1",
+    "DaviRush - 7/10 - Controlador - Ferro 1",
+    "KauanPlay - 4/10 - Sentinela - Ferro 2",
+    "TutuSniper - 10/10 - Duelista - Ferro 2",
+    "BruninAim - 1/10 - Flex - Ferro 2",
+    "LeoHeadshot - 5/10 - Iniciador - Ferro 3",
+    "Zezin - 8/10 - Controlador - Ferro 3",
+    "Joaozin - 3/10 - Sentinela - Ferro 3",
+    "PedrinFPS - 6/10 - Duelista - Ferro 3"
+]
+
+bronze = [
+    "Jeffinhogames - 2/10 - Controlador - Bronze 1",
+    "VictorRush - 9/10 - Iniciador - Bronze 1",
+    "GuiDoValorant - 4/10 - Duelista - Bronze 1",
+    "RafaKills - 7/10 - Duelista - Bronze 2",
+    "HenriqueGG - 1/10 - Flex - Bronze 2",
+    "NandoClutch - 5/10 - Sentinela - Bronze 2",
+    "MatheusX1 - 10/10 - Flex - Bronze 3",
+    "JapaGamer - 3/10 - Controlador - Bronze 3",
+    "LukinhasBR - 6/10 - Duelista - Bronze 3",
+    "Felpszera - 8/10 - Sentinela - Bronze 3"
+]
+
+prata = [
+    "ShadowFPS - 4/10 - Duelista - Prata 1",
+    "Darkzin - 9/10 - Controlador - Prata 1",
+    "Quickzera - 2/10 - Iniciador - Prata 1",
+    "IceWalker - 7/10 - Flex - Prata 2",
+    "NoScopeBR - 1/10 - Sentinela - Prata 2",
+    "ClutchGod - 10/10 - Duelista - Prata 2",
+    "AlphaRush - 5/10 - Controlador - Prata 3",
+    "Smokezera - 3/10 - Flex - Prata 3",
+    "SniperGhost - 8/10 - Iniciador - Prata 3",
+    "Aimzera - 6/10 - Duelista - Prata 3"
+]
+
+ouro = [
+    "RadiantFake - 2/10 - Duelista - Ouro 1",
+    "SilentAim - 9/10 - Controlador - Ouro 1",
+    "GhostPlayer - 5/10 - Sentinela - Ouro 1",
+    "Firezera - 1/10 - Duelista - Ouro 2",
+    "RushMaster - 10/10 - Flex - Ouro 2",
+    "PixelAim - 4/10 - Iniciador - Ouro 2",
+    "NightFPS - 8/10 - Controlador - Ouro 3",
+    "DrakoRush - 3/10 - Duelista - Ouro 3",
+    "MidDiff - 7/10 - Sentinela - Ouro 3",
+    "AceMachine - 6/10 - Flex - Ouro 3"
+]
+
+platina = [
+    "VortexFPS - 10/10 - Duelista - Platina 1",
+    "LunarGhost - 2/10 - Controlador - Platina 1",
+    "ShadowClutch - 8/10 - Sentinela - Platina 1",
+    "NeoAim - 1/10 - Iniciador - Platina 2",
+    "ToxicRush - 7/10 - Flex - Platina 2",
+    "QuickScope - 4/10 - Duelista - Platina 2",
+    "Darkzera - 9/10 - Controlador - Platina 3",
+    "FireGhost - 3/10 - Iniciador - Platina 3",
+    "RushX - 6/10 - Duelista - Platina 3",
+    "SilentFPS - 5/10 - Sentinela - Platina 3"
+]
+
+diamante = [
+    "AlphaGod - 8/10 - Duelista - Diamante 1",
+    "PixelHunter - 1/10 - Controlador - Diamante 1",
+    "DrakoAim - 10/10 - Iniciador - Diamante 1",
+    "SniperKing - 4/10 - Flex - Diamante 2",
+    "NightClutch - 9/10 - Sentinela - Diamante 2",
+    "Frostzera - 2/10 - Duelista - Diamante 2",
+    "InfernoFPS - 7/10 - Controlador - Diamante 3",
+    "GhostRush - 3/10 - Iniciador - Diamante 3",
+    "Clutchzera - 6/10 - Duelista - Diamante 3",
+    "RapidAim - 5/10 - Flex - Diamante 3"
+]
+
+ascendente = [
+    "ViperGod - 1/10 - Duelista - Ascendente 1",
+    "SmokeKing - 9/10 - Controlador - Ascendente 1",
+    "Neonzera - 4/10 - Iniciador - Ascendente 1",
+    "AimMachine - 10/10 - Flex - Ascendente 2",
+    "DarkSniper - 3/10 - Sentinela - Ascendente 2",
+    "RushLegend - 8/10 - Duelista - Ascendente 2",
+    "ClutchPlayer - 2/10 - Controlador - Ascendente 3",
+    "GhostAim - 7/10 - Iniciador - Ascendente 3",
+    "ToxicFPS - 5/10 - Duelista - Ascendente 3",
+    "ShadowKing - 6/10 - Flex - Ascendente 3"
+]
+
+imortal = [
+    "RadiantHunter - 3/10 - Duelista - Imortal 1",
+    "NightmareFPS - 10/10 - Controlador - Imortal 1",
+    "SilentKiller - 2/10 - Sentinela - Imortal 1",
+    "NeoGhost - 9/10 - Iniciador - Imortal 2",
+    "DrakoLegend - 5/10 - Flex - Imortal 2",
+    "QuickAim - 1/10 - Duelista - Imortal 2",
+    "ShadowMachine - 8/10 - Controlador - Imortal 3",
+    "RushPhantom - 4/10 - Iniciador - Imortal 3",
+    "ClutchGodzera - 7/10 - Duelista - Imortal 3",
+    "PixelMaster - 6/10 - Flex - Imortal 3"
+]
+
+radiante = [
+    "TenzFake - 1/10 - Duelista - Radiante",
+    "AspasClone - 8/10 - Controlador - Radiante",
+    "Lesszera - 5/10 - Sentinela - Radiante",
+    "SacyGhost - 10/10 - Iniciador - Radiante",
+    "HeatRush - 3/10 - Flex - Radiante",
+    "MwzeraAim - 9/10 - Duelista - Radiante",
+    "CauanzinFPS - 2/10 - Controlador - Radiante",
+    "QuickPhantom - 7/10 - Iniciador - Radiante",
+    "NeoRadiant - 4/10 - Duelista - Radiante",
+    "ShadowImmortal - 6/10 - Flex - Radiante"
+]
+todosRanks = {
+    1: ferro,
+    2: bronze,
+    3: prata,
+    4: ouro,
+    5: platina,
+    6: diamante,
+    7: ascendente,
+    8: imortal,
+    9: radiante
+}
